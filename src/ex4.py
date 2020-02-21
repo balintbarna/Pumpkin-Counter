1d3a0163305516f1b0a4fa2f4b6aa739b2c7574b
Balint Kovari <bakuv19@student.sdu.dk>
2020-02-21 14:03:03 +0100

    added test function to ex4

--- src/ex4.py
+++ src/ex4.py
@@ -1,5 +1,14 @@
 import cv2
+import constants as cn
+
 
 def median(img, ksize):
+    """
+    ksize should be uneven, e.g.: 9
+    """
     return cv2.medianBlur(img, ksize)
 
+
+if __name__ == '__main__':
+    blurred = median(cn.original_img, 9)
+    cv2.imwrite("output/blurred.jpg", blurred)
