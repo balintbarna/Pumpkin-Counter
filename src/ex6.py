43233532f179b3fd2d463354605106c19846c37a
Balint Kovari <bakuv19@student.sdu.dk>
2020-02-21 15:09:43 +0100

    fix width height

--- src/ex6.py
+++ src/ex6.py
@@ -3,10 +3,13 @@ import math
 
 def calculate_gsd(img, height_meters, fov_deg):
     pixels = img.shape
+    pwidth = pixels[1]
+    pheight = pixels[0]
+    print("resolution: "+str(pwidth)+"x"+str(pheight))
     alpha = math.radians(fov_deg / 2)
     width = height_meters * 2 * math.tan(alpha)
-    ratio = width / pixels[0]
-    height = ratio * pixels[1]
+    ratio = width / pwidth
+    height = ratio * pheight
     ratio *= 1000 # convert to mm/pixel
     print("image_width="+str(width)+" m; image_height="+str(height)+" m; image_ratio="+str(ratio)+" mm/pixel")
     return ratio
